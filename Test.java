c36d9e16326ad06d39efb76994206e2c4946007c
Paddy <paddy77@hushmail.com>
2012-04-05 19:19:58 +0200

    initial commit

---
+++ Test.java
@@ -0,0 +1,47 @@
+import graphics.TileSet;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Test
+{
+
+    public Test()
+        throws Exception
+    {
+         JFrame window = new JFrame();
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setBounds(30, 30, 300, 300);
+            window.getContentPane().add(new MyCanvas());
+            window.setVisible(true);
+    }
+
+     class MyCanvas extends JComponent
+     {
+        BufferedImage img;
+
+        MyCanvas()
+            throws Exception
+        {
+            TileSet tileSet = new TileSet( "res/tileset0000_tiles.jpg", "res/tileset0000.dat" );
+            img = tileSet.getTile( 1 );
+        }
+
+        public void paint( Graphics g )
+        {
+            Graphics2D g2 = ( Graphics2D ) g;
+            g2.drawImage( img, 10, 10, this );
+            g2.finalize();
+        }
+    }
+
+        public static void main( String[] args )
+                throws Exception
+        {
+            new Test();
+        }
+}
---
+++ graphics/TileSet.java
@@ -0,0 +1,98 @@
+package graphics;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class TileSet
+{
+    private BufferedImage img = null;
+
+    private Color backgroundColor;
+
+    private HashMap<Integer,TileInfo> tileInfos = new HashMap<Integer,TileInfo>();
+
+    public TileSet( String imagePath, String infoPath )
+        throws IOException
+    {
+        img = ImageIO.read( new File( imagePath ) );
+        readTileInfo( infoPath );
+    }
+
+    private void readTileInfo( String infoPath )
+        throws IOException
+    {
+        File file = new File( infoPath );
+        BufferedReader bufRdr = new BufferedReader( new FileReader( file ) );
+        String line = null;
+        line = bufRdr.readLine();
+        StringTokenizer st = new StringTokenizer( line, "\t" );
+        int red = Integer.parseInt( st.nextToken() );
+        int green = Integer.parseInt( st.nextToken() );
+        int blue = Integer.parseInt( st.nextToken() );
+        backgroundColor = new Color( red, green, blue );
+        while ( ( line = bufRdr.readLine() ) != null )
+        {
+            st = new StringTokenizer( line, "\t" );
+            int id = Integer.parseInt( st.nextToken() );
+            TileInfo tileInfo = new TileInfo();
+            tileInfo.id = id;
+            tileInfo.obstacle = Integer.parseInt( st.nextToken() );
+            tileInfo.animation = Integer.parseInt( st.nextToken() );
+            tileInfo.default_layer = Integer.parseInt( st.nextToken() );
+
+            if ( /*tileInfo.animation != 1 &&*/ tileInfo.animation != 5 )
+            {
+                tileInfo.x = Integer.parseInt( st.nextToken() );
+                tileInfo.y = Integer.parseInt( st.nextToken() );
+                tileInfo.width = Integer.parseInt( st.nextToken() );
+                tileInfo.height = Integer.parseInt( st.nextToken() );
+            }
+            else
+            {
+                tileInfo.sequence = Integer.parseInt( st.nextToken() );
+                tileInfo.width = Integer.parseInt( st.nextToken() );
+                tileInfo.height = Integer.parseInt( st.nextToken() );
+                tileInfo.x1 = Integer.parseInt( st.nextToken() );
+                tileInfo.y1 = Integer.parseInt( st.nextToken() );
+                tileInfo.x2 = Integer.parseInt( st.nextToken() );
+                tileInfo.y2 = Integer.parseInt( st.nextToken() );
+                tileInfo.x3 = Integer.parseInt( st.nextToken() );
+                tileInfo.y3 = Integer.parseInt( st.nextToken() );
+            }
+            tileInfos.put( id, tileInfo );
+        }
+        bufRdr.close();
+    }
+
+    public BufferedImage getTile( int id )
+    {
+        TileInfo tileInfo = tileInfos.get( id );
+        return img.getSubimage( tileInfo.x, tileInfo.y, tileInfo.width, tileInfo.height );
+    }
+
+    class TileInfo
+    {
+        int id;
+        int obstacle;
+        int animation;
+        int default_layer;
+        int x;
+        int y;
+        int width;
+        int height;
+        int sequence;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        int x3;
+        int y3;
+    }
+}
